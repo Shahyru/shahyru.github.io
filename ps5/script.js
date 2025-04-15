a9c829871baac02177643f6756fc3f3bcf3d7fd1
Shahyru <92292396+Shahyru@users.noreply.github.com>
2025-04-15 16:51:43 +0500

    Update script.js

--- ps5/script.js
+++ ps5/script.js
@@ -25,7 +25,7 @@ const draw = () => {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = '#757171';  // Matrix green color
+    ctx.fillStyle = '#524d4d';  // Matrix green color
     ctx.font = fontSize + 'px monospace';
 
     for (let i = 0; i < rainDrops.length; i++) {
